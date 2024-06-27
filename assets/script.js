4b175c0dd62f0e1020b24c9553caee004dd67f78
VanceGoblin <zvkubajak@gmail.com>
2024-06-27 14:21:26 -0500

    Added HTML Table, CSS, and Add & Remove Buttons

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// Make a function that adds a new row to the table. The function should be called by the Add Stand Button.
+
+// Make a function that removes the last row from the table. The function should be called by the Remove Stand Button.
+
+// Make a function that moves the value of the lifeguard input field down to the next row. If the value is already in the last row, it should be moved to the first row. This function should occur every 15 minutes.
+
+function addStand () {
+
+}
+
+function removeStand () {
+
+}
